1840f64fe19a9f52f2ec044a3e87e0c85319a04f
mr.Ruslaaan <annaev.ruslan@gmail.com>
2020-10-07 22:41:07 +0300

    smth

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,6 +1,17 @@
-﻿namespace MailSender
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MailSender
 {
-    public partial class App 
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
     {
     }
 }
--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,11 +1,30 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+<<<<<<< HEAD
 using MailSender.lib;
+=======
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+>>>>>>> parent of f2ee804... Merge remote-tracking branch 'origin/lesson-1'
 
 namespace MailSender
 {
-    public partial class MainWindow
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
+<<<<<<< HEAD
         public MainWindow() => InitializeComponent();
         private void ExitButtonClick(object sender, System.Windows.RoutedEventArgs e)
         {
@@ -15,6 +34,11 @@ namespace MailSender
         private void btnClock_Click(object sender, RoutedEventArgs e)
         {
             tabContol.SelectedItem = tabPlanner;            
+=======
+        public MainWindow()
+        {
+            InitializeComponent();
+>>>>>>> parent of f2ee804... Merge remote-tracking branch 'origin/lesson-1'
         }
     }
 }
